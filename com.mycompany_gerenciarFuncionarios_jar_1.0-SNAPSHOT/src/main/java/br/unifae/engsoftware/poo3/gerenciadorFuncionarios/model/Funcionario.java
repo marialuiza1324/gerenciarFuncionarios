1a3013d24ccbcc168df420dd3a8654331a68c14e
marialuiza1324 <marialuiza.nerypinto@gmail.com>
2025-03-29 22:52:45 -0300

    init backend

---
+++ com.mycompany_gerenciarFuncionarios_jar_1.0-SNAPSHOT/src/main/java/br/unifae/engsoftware/poo3/gerenciadorFuncionarios/model/Funcionario.java
@@ -0,0 +1,72 @@
+package br.unifae.engsoftware.poo3.gerenciadorFuncionarios.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Maria Eduarda de Oliveira Furini e Maria Luiza Nery Pinto
+ */
+public class Funcionario {
+    public String codigo;
+    private String nome;
+    private double salario;
+    
+    public Funcionario(String codigo, String nome, double salario){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this.codigo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Funcionario other = (Funcionario) obj;
+        return Objects.equals(this.codigo, other.codigo);
+    }
+    
+    @Override
+    public String toString(){
+        return codigo + "\n" +
+                nome + "\n" +
+                salario + "\n";
+    }
+}
